82be9af882725c2f59c516757a70d79067ee28b1
fershad <27988517+fershad@users.noreply.github.com>
2025-06-30 12:46:33 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-export default {
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
   build: {
     outDir: "dist",
     assetsDir: ".",
     emitAssets: true,
+    lib: {
+      entry: resolve(__dirname, "src/index.js"),
+      name: "GawInfoBar",
+      fileName: (format) => `gaw-info-bar.${format === "es" ? "js" : "cjs"}`,
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: ["lit"],
+      output: {
+        globals: {
+          lit: "Lit",
+        },
+      },
+    },
   },
-};
+});
